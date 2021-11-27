1c1d11d009a34208acf3c259f7f1f8599efaa92b
Silixy <95109508+Silixy@users.noreply.github.com>
2021-11-26 20:01:35 -0600

    Update @echo off.bat

--- @echo off.bat
+++ @echo off.bat
@@ -1,5 +1,5 @@
 @echo off
-if exist %TEMP%\loggedin.tmp (-Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Already Logged in', 'Success', 'Ok', [System.Windows.Forms.MessageBoxIcon]::Information);}" > %TEMP%\already.tmp)
+if exist %TEMP%\loggedin.tmp (powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Already Logged in', 'Success', 'Ok', [System.Windows.Forms.MessageBoxIcon]::Information);}" > %TEMP%\already.tmp)
 powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Do you have an Account?', 'Account', 'YesNoCancel', [System.Windows.Forms.MessageBoxIcon]::Information);}" > %TEMP%\acc.tmp
 set /p account=<%TEMP%\acc.tmp
 if /i %account%== no (powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Would you like to Sign-Up?', 'Sign-Up', 'YesNoCancel', [System.Windows.Forms.MessageBoxIcon]::Information);}" > %TEMP%\sup.tmp)
@@ -8,11 +8,11 @@ if /i %signup%== yes (powershell -Command "& {Add-Type -AssemblyName Microsoft.V
 set /p username=<%TEMP%\username.tmp
 if /i %signup%== yes (powershell -Command "& {Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('Enter Password:', 'Password')}" > %TEMP%\password.tmp)
 set /p password=<%TEMP%\password.tmp
-if /i %signup% = yes (powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Restart and Login', 'Success', 'Ok', [System.Windows.Forms.MessageBoxIcon]::Information);}" > %TEMP%\ok.tmp)
-set /p ok=<%TEMP%\ok.tmp
 if /i %account%== yes (powershell -Command "& {Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('Enter Username:', 'Username')}" > %TEMP%\loginu.tmp)
-set /p Loginu=<%TEMP%\loginusername.tmp
-if %Loginu%== %username% (powershell -Command "& {Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('Enter Password:', 'Password')}" > %TEMP%\Loginp.tmp) else (-Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('%loginu% was not found.', 'Wrong Credentials', 'Ok', [System.Windows.Forms.MessageBoxIcon]::Information);}" > %TEMP%\wrong.tmp)
+set /p loginu=<%TEMP%\loginu.tmp
+if %loginu%== %username% (powershell -Command "& {Add-Type -AssemblyName Microsoft.VisualBasic; [Microsoft.VisualBasic.Interaction]::InputBox('Enter Password:', 'Password')}" > %TEMP%\loginp.tmp)
 set /p Loginp=<%TEMP%\Loginp.tmp
-if %Loginp%== %password% (-Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Login Success', 'Success', 'Ok', [System.Windows.Forms.MessageBoxIcon]::Information);}" > %TEMP%\loggedin.tmp) else (-Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Login Invalid!', 'Wrong Password', 'Ok', [System.Windows.Forms.MessageBoxIcon]::Information);}" > %TEMP%\error.tmp)
+if NOT %loginp%== %password% (powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Login Invalid', 'Invalid', 'Ok', [System.Windows.Forms.MessageBoxIcon]::Information);}" > %TEMP%\wrong.tmp)
+set /p wrong=<%TEMP%\wrong.tmp
+if %loginp%== %password% (powershell -Command "& {Add-Type -AssemblyName System.Windows.Forms; [System.Windows.Forms.MessageBox]::Show('Login Success', 'Success', 'Ok', [System.Windows.Forms.MessageBoxIcon]::Information);}" > %TEMP%\loggedin.tmp)
 set /p loggedin=<%TEMP%\loggedin.tmp
